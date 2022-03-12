5284823d916b735a4d7d12edd56f605c170190c5
osqui1978 <99231492+osqui1978@users.noreply.github.com>
2022-03-12 19:51:37 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -109,7 +109,14 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+ resultado = num % 2;
+ if ( resultado == 0) {
+   return true;
+ }
+ else {
+   return false;
+ }
+
 }
 
 function esImpar(num) {
